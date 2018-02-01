82b23eba5b16a2e64ecb2b10833ead17fcbaf4c3
Mateusz <matrop002@utp.edu.pl>
2018-02-01 21:30:02 +0100

    Liczba

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 int main () {
 	
 	std::cout << "Hello World" << std::endl;
-	return 0;
+	std::cout << "Podaj Liczbę" << std::endl;
+	int a;
+	if (a>10) std::cout << "Liczba większa od 10" << std::endl;
+	else std::cout <<"Liczba mniejsza od 10" << std::endl;
+
+return 0;
 	
 }
